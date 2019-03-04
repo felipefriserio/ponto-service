59f460491684ec370e475143d1819b137429eed7
felipefriserio <felipefriserio@gmail.com>
2019-03-04 10:35:15 -0300

    alteracoes

--- src/main/java/br/com/pontoService/model/Ponto.java
+++ src/main/java/br/com/pontoService/model/Ponto.java
@@ -9,16 +9,8 @@ import org.springframework.data.mongodb.core.mapping.Document;
 public class Ponto {
 	
 	@Id 
-	private String id;
 	private LocalDateTime marcacao;
 	
-	
-	public String getId() {
-		return id;
-	}
-	public void setId(String id) {
-		this.id = id;
-	}
 	public LocalDateTime getMarcacao() {
 		return marcacao;
 	}
